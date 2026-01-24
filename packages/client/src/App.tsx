7946f1ea910274ae6317ab0c6988f914cd7e69e6
neil-feris <neil.feris@gmail.com>
2026-01-25 00:20:40 +0200

    feat: US-009 - Create Flashcard Setup Page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,6 +8,7 @@ import { ExamSetup } from './components/exam/ExamSetup';
 import { ExamContainer } from './components/exam/ExamContainer';
 import { ExamReview } from './components/exam/ExamReview';
 import { StudyHub } from './components/study/StudyHub';
+import { FlashcardSetup } from './components/study/flashcards';
 import { LearningPathDetail } from './components/study/learning-path/LearningPathDetail';
 import { Review } from './components/review/Review';
 import { Settings } from './components/settings/Settings';
@@ -144,6 +145,18 @@ function App() {
               <AppShell>
                 <RouteErrorBoundary>
                   <StudyHub />
+                </RouteErrorBoundary>
+              </AppShell>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/study/flashcards"
+          element={
+            <ProtectedRoute>
+              <AppShell>
+                <RouteErrorBoundary>
+                  <FlashcardSetup />
                 </RouteErrorBoundary>
               </AppShell>
             </ProtectedRoute>
